ef183687fbb9bca70890b5aa993547d5b8a94b3f
Christophe Silhouette <c.silhouette@stuart.com>
2021-05-03 10:13:21 +0200

    clean code and sample files for demo

---
+++ config.py
@@ -0,0 +1,3 @@
+PATH_TO_FILE = "/Users/c.silhouette/dev/monitoring/tests/mocks/datalake.prod.stuart.com/raw/kafka" \
+               "/business_events/delivery_canceled/year=2021/month=04/day=28/hour={" \
+               "}/zone=paris/biz-paris-0-0809009463.csv"
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 import pandas as pd
 from dataclasses import dataclass
+import time
 from fire import Fire
-import boto3
+from config import PATH_TO_FILE
 
 if __name__ == '__main__':
 
@@ -10,47 +11,67 @@ if __name__ == '__main__':
     # 2) Faire un makefile : lance le prometheus, le gateway,
     # lancer le script qui va populate le prometheus a travers la gateway
 
-
-    data = '# TYPE total_count counter total_count{layer="raw", source="business_events", table="delivery_canceled"} 20'
-
-    response = requests.post('http://localhost:9091/metrics/job/datalake/', data=data)
-
-
     @dataclass()
     class Metric:
 
         name: str
+        type: str
         value: int
+        labels: dict
 
     class Loader:
+        @staticmethod
+        def load(path: str) -> pd.DataFrame:
+            return pd.read_csv(path)
 
-        def load(self, path_to_s3: str) -> pd.DataFrame:
-            pass
 
     class ComputeMetrics:
+        @staticmethod
+        def compute(df: pd.DataFrame, name: str, type: str, labels: dict) -> Metric:
+            if name == 'total_count':
+                return Metric(name=name, type=type, labels=labels, value=df['first_name'].shape[0])
 
-        def compute(self, df: pd.DataFrame) -> Metric:
-            return Metric(name='total_count', value=df.count())
+    def prom_query_factory(metric: Metric):
+
+        return '# TYPE {metric_name} {metric_type}\n{metric_name}{labels} {valeur}\n'.format(
+            metric_name=metric.name,
+            metric_type=metric.type,
+            labels=metric.labels,
+            valeur=str(metric.value))
 
     class PrometheusClient:
 
         def __init__(self):
             self.prometheus_client = None
 
-        def send_metric(self, metric: Metric):
-            pass
+        @staticmethod
+        def send_metric(metric: Metric):
 
+            data = prom_query_factory(metric)
 
-    def main(path_to_s3: str, metric_table: str):
+            response = requests.post('http://localhost:9091/metrics/job/datalake/', data=data)
+            print(response)
+
+    def main(path_to_s3: str = None, metric_table: str = None):
         """
         My doc
         """
-        loader = Loader()
-        compute_metrics = ComputeMetrics()
+        for hour in range(19, 24):
 
-        df = loader.load(path_to_s3)
-        metric = compute_metrics.compute(df)
-        PrometheusClient.send_metric(metric)
+            path = PATH_TO_FILE.format(hour)
+
+            df = Loader.load(path=path)
+
+            metric = ComputeMetrics. \
+                compute(df,
+                        name='total_count',
+                        type='gauge',
+                        labels="{layer=\"raw\", source=\"business_events\", table=\"delivery_canceled\"}")
+
+            PrometheusClient.send_metric(metric=metric)
+            time.sleep(10)
+
 
     if __name__ == '__main__':
-        Fire(main)
+        main()
+        #Fire(main)
